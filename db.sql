5fff364d0d167ca7c2aac265f341992ff4da8027
Tushar <tusharpokhariyal@gmail.com>
2026-04-10 15:05:00 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE cms (
+    Case_id VARCHAR(20) PRIMARY KEY,
+    Criminal_no VARCHAR(20),
+    Type_of_Crime VARCHAR(100),
+    Name VARCHAR(100),
+    Aliases VARCHAR(100),
+    Fathers_Name VARCHAR(100),
+    Date_of_Arrest VARCHAR(50),
+    DateofCrime VARCHAR(50),
+    Birthmarks VARCHAR(100),
+    Age VARCHAR(10),
+    Occupation VARCHAR(100),
+    Nationality VARCHAR(100),
+    Gender VARCHAR(10),
+    Wanted VARCHAR(10)
+);
